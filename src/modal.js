dee42f859c24c1e316bf59f17132d93a2fe600ef
maryhuziuk <116351558+maryhuziuk@users.noreply.github.com>
2023-01-15 18:52:22 +0200

    create modals

--- src/modal.js
+++ src/modal.js
@@ -7,6 +7,18 @@
     openModalBuynowBtn: document.querySelector("[modal-buynow-open]"),
     closeModalBuynowBtn: document.querySelector("[modal-buynow-close]"),
     modalBuynow: document.querySelector("[modal-buynow]"),
+
+    openModalLocationBtn: document.querySelector("[modal-location-open]"),
+    closeModalLocationBtn: document.querySelector("[modal-location-close]"),
+    modalLocation: document.querySelector("[modal-location]"),
+
+    openModalFranchiseBtn: document.querySelector("[modal-franchise-open]"),
+    closeModalFranchiseBtn: document.querySelector("[modal-franchise-close]"),
+    modalFranchise: document.querySelector("[modal-franchise]"),
+    
+    openModalReadMoreBtn: document.querySelector("[modal-read-more-open]"),
+    closeModalReadMoreBtn: document.querySelector("[modal-read-more-close]"),
+    modalReadMore: document.querySelector("[modal-read-more]"),
   };
 
   refs.openModalProductBtn.addEventListener("click", toggleModalProduct);
@@ -14,14 +26,36 @@
 
    refs.openModalBuynowBtn.addEventListener("click", toggleModalBuynow);
   refs.closeModalBuynowBtn.addEventListener("click", toggleModalBuynow);
+  
+   refs.openModalLocationBtn.addEventListener("click", toggleModalLocation);
+  refs.closeModalLocationBtn.addEventListener("click", toggleModalLocation);
+
+  refs.openModalFranchiseBtn.addEventListener("click", toggleModalFranchise);
+  refs.closeModalFranchiseBtn.addEventListener("click", toggleModalFranchise);
+
+  refs.openModalReadMoreBtn.addEventListener("click", toggleModalReadMore);
+  refs.closeModalReadMoreBtn.addEventListener("click", toggleModalReadMore);
+
 
   function toggleModalProduct() {
     refs.modalProduct.classList.toggle("is-hidden");
     
   }
+  
   function toggleModalBuynow() {
      refs.modalBuynow.classList.toggle("is-hidden");
   }
   
+  function toggleModalLocation() {
+     refs.modalLocation.classList.toggle("is-hidden");
+  }
+  
+  function toggleModalFranchise() {
+     refs.modalFranchise.classList.toggle("is-hidden");
+  }
+
+    function toggleModalReadMore() {
+     refs.modalReadMore.classList.toggle("is-hidden");
+  }
 
 })();
